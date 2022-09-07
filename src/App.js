3e5dad37b877f94ce71618e28e90371eab81b746
Mihir2423 <mihirraj444@gmail.com>
2022-09-07 17:42:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import SearchPage from "./Pages/SearchPage"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import InfoPage from "./Pages/InfoPage"
 
+
 export default function App() {
     return (
         <BrowserRouter>
+
             <Routes>
                 <Route path='/' element={<SearchPage />} />
-                <Route path='userInfo/:id' element={<InfoPage />} />
+            </Routes>
+            <Routes>
+                <Route exact path='userInfo/:id' element={<InfoPage />} />
             </Routes>
         </BrowserRouter>
     )
