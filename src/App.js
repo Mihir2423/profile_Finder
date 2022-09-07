f1b98000210d52bddda5162b19101894fabacd00
Mihir2423 <mihirraj444@gmail.com>
2022-09-07 11:26:14 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import SearchPage from "./Pages/SearchPage"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import InfoPage from "./Pages/InfoPage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<SearchPage />} />
+                <Route path='/:id' element={<InfoPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
-export default App;
